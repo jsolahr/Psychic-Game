80109b1f0a8ddbe63899ac2f815bca93cd18dfc3
jsolahr <51134556+jsolahr@users.noreply.github.com>
2019-07-16 22:41:47 -0500

    updated notes in javascript

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,5 @@
 
-//Psychic Game
+//Psychic Game Variables
 var computerChoices = "abcdefghijklmnopqrstuvwxyz"
 var wins = 0;
 var losses = 0;
@@ -7,7 +7,7 @@ var guesses = 11;
 var computerGuess = computerGuess;
 var usedArray = [];
 
-// Create variables that hold references to the places in the HTML where we want to display things.
+// Created variables that hold references to the places in the HTML where I want to display things.
 var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text")
 var winsText = document.getElementById("wins-text")
@@ -15,16 +15,16 @@ var lettersguessed = document.getElementById("lettersguessed-text")
 var lossesText = document.getElementById("losses-text")
 var guessesleftText = document.getElementById("guessesleft-text")
 
-//Generates Random Computer Letter
+//Generates random Computer Letter
 computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 console.log(computerGuess);
 
-//Uses random whole number generated above to select a random letter from the array and assigns it to the var computerChoices
+//Created a function that uses random whole number generated above to select a random letter from the array and assigns it to the var computerChoice
 function myGuess() {
       computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-      console.log(computerGuess);
+console.log(computerGuess);
 }
-//Capture player Input
+//Captures my input
 document.onkeyup = function(event) {
     var userGuess = event.key;
 
@@ -33,15 +33,15 @@ if (userGuess === computerGuess) {
 wins++;
 guesses = 11;
 usedArray = [];
-alert("Congratulations! You guessed " +computerGuess + " which is correct!");
+alert("Congratulations! You guessed " + computerGuess + " which is correct!");
 console.log(computerGuess)
 }
-//When I guess a wrong letter guesses goes down by 1
+//When I guess a wrong letter guesses goes down by 1 
 myGuess();
 if (userGuess !== computerGuess) {
 guesses--;
 }
-//When I have no more attempts, add a loss and reset guesses to zero
+//When I have no more attempts, add a loss and reset guesses to zero 
 if (guesses == 0) {
 losses++;
 usedArray = []
@@ -50,7 +50,7 @@ guesses = 11;
 ///This prevents a repeated letter from printing
 if (usedArray.indexOf(userGuess) >= 0) {
 
-//This stores my incorrect letters to my array and prints it to HTML
+//The array has my incorrect letters to my array and prints it to HTML
 } else {
     usedArray.push(userGuess);
     document.getElementById('playerGuess').innerHTML = usedArray;
